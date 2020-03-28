dab11a52c257d630c6ea9d34fbad623433f88177
grafiz <grafiz.creation@gmail.com>
2020-03-29 00:14:51 +0100

    création de l'architecture de base du site

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grafiz-site
 le site de grafiz
+
+*npm start* => surveille la compilation de SASS
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My custom Bulma website</title>
+    <link rel="stylesheet" href="../sass/style.css">
+  </head>
+  <body>
+     <h1 class="title is-primary">Hello world</h1>
+     <button class="button is-primary">Button</button>
+  </body>
+</html>
