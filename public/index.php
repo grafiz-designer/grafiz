6427fa74daa40232543b42931a1780ff872b10a4
grafiz <grafiz.creation@gmail.com>
2020-04-04 11:45:00 +0200

    MAJ du routeur

--- public/index.php
+++ public/index.php
@@ -9,20 +9,19 @@ require "../_config.php";
 // je charge "autoload" pour charger dynamiquement la classe que l'on souhaite.
 require ROOT . "/vendor/autoload.php";
 
-debug($_GET);
 $request = $_GET['action'];
 
 
 if(isset($request) && $request === "home"){
     require CONTROLLER . "/home.php";
 }else if (isset($request) && $request === "works") {
-    require VIEW . "/works.php";
+    require CONTROLLER . "/works.php";
 }else if (isset($request) && $request === "about"){
-    require VIEW . "/about.php";
+    require CONTROLLER . "/about.php";
 }else if (isset($request) && $request === "contact"){
-    require VIEW . "/contact.php";
+    require CONTROLLER . "/contact.php";
 }else{
-    require VIEW . "/404.php";
+    require CONTROLLER . "/404.php";
 }
 
 
